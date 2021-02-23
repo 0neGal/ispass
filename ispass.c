ee12ff9c76049550e16a8795eca6d0d3b889e05d
0neGal <contact@0neguy.com>
2021-02-23 19:28:45 +0100

    Why am I doing this.

--- ispass.c
+++ ispass.c
@@ -3,18 +3,16 @@
 #include <shadow.h>
 #include <crypt.h>
 
-int chk(const char *plain, const char *cryptp) {
-	return strcmp(crypt(plain, cryptp), cryptp) == 0;
-};;
+int chk(const char *plain, const char *cryptp) { return strcmp(crypt(plain, cryptp), cryptp) == 0; };;
 
 int main(int argc, char* argv[]) {
 	struct spwd* spwd;
 
-	if (argc != 3) {return EXIT_FAILURE;};
+	if (argc != 3) { return 1; };
 	spwd = getspnam(argv[1]);
-	if (spwd == NULL) {return EXIT_FAILURE;};
-	if (!chk(argv[2], spwd->sp_pwdp)) {return EXIT_FAILURE;};
+	if (spwd == NULL) { return 1; };
+	if (!chk(argv[2], spwd->sp_pwdp)) { return 1; };
 
-	return EXIT_SUCCESS;
+	return 0;
 };
 
