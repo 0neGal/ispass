6ad8382c26ba3b7835aecb8e15dff1b9b5d4a5b7
0neGal <contact@0neguy.com>
2021-02-23 19:27:02 +0100

    Simplication of the code, just because.

--- ispass.c
+++ ispass.c
@@ -3,27 +3,17 @@
 #include <shadow.h>
 #include <crypt.h>
 
-int chkPass(const char *plain, const char *cryptp) {
+int chk(const char *plain, const char *cryptp) {
 	return strcmp(crypt(plain, cryptp), cryptp) == 0;
 };;
-
 
 int main(int argc, char* argv[]) {
 	struct spwd* spwd;
 
-	if (argc != 3) {
-		return EXIT_FAILURE;
-	};
-
+	if (argc != 3) {return EXIT_FAILURE;};
 	spwd = getspnam(argv[1]);
-
-	if (spwd == NULL) {
-		return EXIT_FAILURE;
-	};
-
-	if (!chkPass(argv[2], spwd->sp_pwdp)) {
-		return EXIT_FAILURE;
-	};
+	if (spwd == NULL) {return EXIT_FAILURE;};
+	if (!chk(argv[2], spwd->sp_pwdp)) {return EXIT_FAILURE;};
 
 	return EXIT_SUCCESS;
 };
